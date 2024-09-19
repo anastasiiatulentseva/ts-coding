3b39c0f04f11c839938a149930e6d528a334ae37
Anastasiia Tulentseva <anastasiia.tulentseva@gmail.com>
2024-09-20 00:21:00 +0100

    add styling to the board

--- src/components/TicTacToe/Board.tsx
+++ src/components/TicTacToe/Board.tsx
@@ -1,5 +1,20 @@
+import "./board.scss";
+
 function Board() {
-  return <div>here will be the board</div>;
+  return (
+    <div className="board-container">
+      <div className="game-header">Tic Tac Toe</div>
+      <div className="current-player">Current Player: N</div>
+      <div className="draw-label">Congratulations you!</div>
+      <div className="board">
+        {Array.from({ length: 9 }).map((_, index) => (
+          <div key={index} className="cell --no-hover--">
+            XO
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Board;
